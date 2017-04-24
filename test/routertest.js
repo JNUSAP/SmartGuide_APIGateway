f6ec5bd78151e3e9283b22478ec78a47a6ccc3a1
HardPlant <abc7988se@naver.com>
2017-04-24 19:20:04 +0900

    server test added

---
+++ test/routertest.js
@@ -0,0 +1,12 @@
+const chai = require('chai'); // assert, should, expect
+var assert = chai.assert;
+const chaiHttp = require('chai-http');
+const server = require('../app.js');
+const should = chai.should();
+chai.use(chaiHttp);
+
+describe('라우터 테스트', function(){
+    describe('',function(){
+        
+    });
+});
