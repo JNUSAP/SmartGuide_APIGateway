abc60583b59ed84d9ac235e0c4cd4d198246783c
HardPlant <abc7988se@naver.com>
2017-05-08 16:51:21 +0900

    카카오톡 응답 API 요구조건 충족

--- router.js
+++ router.js
@@ -4,4 +4,34 @@ exports.init = function(app) {
             res.setHeader('Content-Type', 'application/json');
             res.send(JSON.stringify(flag['xormagic']));
         });
+    app.get('/keyboard',
+        function(req, res) {
+            res.setHeader('Content-Type', 'application/json');
+            res.send(JSON.stringify({ "type": "text" }));
+        });
+    app.post('/message',
+        function(req, res) {
+            res.setHeader('Content-Type', 'application/json');
+            res.send(JSON.stringify({
+                    "message": {
+                        "text": "테스트 중입니다.",
+                        "photo": {
+                            "url": "",
+                            "width": "",
+                            "height": ""
+                        },
+                        "message_button": {
+
+                        }
+                    }
+                } // 임시 메시지. MessageProvider.js로
+            ));
+        });
+    app.get('/keyboard',
+        function(req, res) {
+            res.setHeader('Content-Type', 'application/json');
+            res.send(JSON.stringify({ "type": "text" }));
+        });
+
+
 };
