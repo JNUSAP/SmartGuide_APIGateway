7281ba7c5c47e43b473598a31810189ff2636cd0
HardPlant <abc7988se@naver.com>
2017-05-12 17:35:02 +0900

    kakao response format

--- BdgSearchModule.js
+++ BdgSearchModule.js
@@ -5,15 +5,15 @@ exports.getKakaoResponse = function() {
     return {
         "message":{
                 "text": "안녕하세요.",
-            "photo": {
-                "url": "https://hello.photo.src",
-                "width": 640,
-                "height": 480
-            },
-            "message_button": {
-                "label": "반갑습니다.",
-                "url": "http://hello.world.com/example"
-            }
+                "photo": {
+                    "url": "https://hello.photo.src",
+                    "width": 640,
+                    "height": 480
+                },
+                "message_button": {
+                    "label": "반갑습니다.",
+                    "url": "http://hello.world.com/example"
+                }
         }
     };
 };
