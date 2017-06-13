0c900ab594012d72d7c735b7ff12196493781e8d
unknown <120897@live.jnu.ac.kr>
2017-06-14 08:49:12 +0900

    added comments

--- kakaotalkresponse.js
+++ kakaotalkresponse.js
@@ -47,28 +47,4 @@ function FailedMessageButton() {
     this.label = "제안하기";
     this.url = config.host + "/failed";
 }
-/*
-{
-  "message": {
-    "text": "귀하의 차량이 성공적으로 등록되었습니다. 축하합니다!",
-    "photo": {
-      "url": "https://photo.src",
-      "width": 640,
-      "height": 480
-    },
-    "message_button": {
-      "label": "주유 쿠폰받기",
-      "url": "https://coupon/url"
-    }
-  },
-  "keyboard": {
-    "type": "buttons",
-    "buttons": [
-      "처음으로",
-      "다시 등록하기",
-      "취소하기"
-    ]
-  }
-}
-*/
 module.exports = KakaoResponse;
