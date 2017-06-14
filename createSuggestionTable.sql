98da22665bfe2ae0ed62a31c65c54658128d6ed2
HardPlant <abc7988se@naver.com>
2017-06-14 12:17:27 +0900

    sql

---
+++ createSuggestionTable.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `mainBuilding`.`suggestion` (
+  `idsuggestion` INT NOT NULL,
+  `suggestionTitle` VARCHAR(60) NULL,
+  `suggestionText` VARCHAR(150) NULL,
+  PRIMARY KEY (`idsuggestion`));
